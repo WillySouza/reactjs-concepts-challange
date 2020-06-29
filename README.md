9ac0b90802806f6322157c8cad9de2d10f7b1ee6
Willy <willysouza@outlook.com>
2020-06-29 08:49:39 -0300

    tasks solutions implemented

---
+++ README.md
@@ -0,0 +1,23 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+## 🚀 About
+
+In this challenge, we created a basic frontend to train what we have learned so far in ReactJS. [Check it here](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs).
+
+:memo: **Tasks**
+- [x] Should be able to add new repository
+- [x] Should be able to remove repository
+
+:bulb: **Technologies used so far**
+- Javascript;
+- Node.js;
+- Express;
+
+:mag_right: **Code check**
+```
+git clone https://github.com/WillySouza/reactjs-concepts-challange.git
+yarn
+yarn test 
+```
+---
+Made with 💜 by [me](https://www.linkedin.com/in/w1llysouza/)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Willy Souza"
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    const response = await api.delete(`/repositories/${id}`);
+
+    if (response.status !== 204){
+      return alert("Repository couldn't be deleted!");
+    }
+
+    const index = repositories.findIndex(repository => repository.id === id );
+    repositories.splice(index);
+    setRepositories([...repositories]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => <li key={repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
